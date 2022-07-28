a19d26d14b31065c31f56cec5fe4134bcc2b19c0
linus-edor <edorlinus@gmail.com>
2022-07-28 18:46:35 +0000

    timestamp added to the db

--- src/main/java/com/example/sim_registration_v8/models/SIMRegistrationModel.java
+++ src/main/java/com/example/sim_registration_v8/models/SIMRegistrationModel.java
@@ -9,14 +9,12 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "SIM_REGISTRATION", uniqueConstraints = @UniqueConstraint(columnNames = "transaction_id"))
